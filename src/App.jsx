39b99a6131ee97d6cc4b68146e664b5ddad63418
Pro-Programmer38 <khursheedahmed786k@gmail.com>
2023-11-06 12:46:25 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer, Hero } from "./sections";
+
+import Nav from "./components/Nav";
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+
+    <section className="padding">
+      <PopularProducts />
+    </section>
+
+    <section className="padding">
+      <SuperQuality />
+    </section>
+
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+)
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "/Users/macbook/Desktop/Nike/src/sections/Hero.jsx";
+import CustomerReviews from "../sections/CustomerReviews";
+import Footer from "../sections/Footer";
+import PopularProducts from "../sections/PopularProducts";
+import Services from "../sections/Services";
+import SpecialOffers from "../sections/SpecialOffers";
+import Subscribe from "../sections/Subscribe";
+import SuperQuality from "../sections/SuperQuality";
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality
+}
