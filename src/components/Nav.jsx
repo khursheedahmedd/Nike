5c5b07a894766faa08e14e54396a246878229156
Pro-Programmer38 <khursheedahmed786k@gmail.com>
2023-12-01 23:38:16 +0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -41,45 +41,45 @@ const Nav = () => {
                     />
                 </a>
                 {isMobile ? (
-                    // <div className="max-lg:hidden">
-                    //     <img
-                    //         src={hamburger}
-                    //         alt="hamburger"
-                    //         width={25}
-                    //         height={25}
-                    //         onClick={toggleMobileMenu}
-                    //         style={{ cursor: 'pointer' }}
-                    //     />
-                    // </div>
-                ): (
-                        <ul className = "flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                    <div className="max-lg:hidden">
+                        <img
+                            src={hamburger}
+                            alt="hamburger"
+                            width={25}
+                            height={25}
+                            onClick={toggleMobileMenu}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    </div>
+                ) : (
+                    <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
                         {navLinks.map((item) => (
-                <li key={item.label} >
-                    <a
-                        href={item.href}
-                        className="font-monts errat leading-normal text-lg text-slate-gray"
-                    >
-                        {item.label}
-                    </a>
-                </li>
+                            <li key={item.label} >
+                                <a
+                                    href={item.href}
+                                    className="font-monts errat leading-normal text-lg text-slate-gray"
+                                >
+                                    {item.label}
+                                </a>
+                            </li>
                         ))}
-            </ul>
+                    </ul>
                 )}
-            {isMobile && (
-                <ul className={`flex-1 flex justify-center items-center gap-16 max-lg:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
-                    {navLinks.map((item) => (
-                        <li key={item.label} >
-                            <a
-                                href={item.href}
-                                className="font-monts errat leading-normal text-lg text-slate-gray"
-                            >
-                                {item.label}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </nav>
+                {isMobile && (
+                    <ul className={`flex-1 flex justify-center items-center gap-16 max-lg:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
+                        {navLinks.map((item) => (
+                            <li key={item.label} >
+                                <a
+                                    href={item.href}
+                                    className="font-monts errat leading-normal text-lg text-slate-gray"
+                                >
+                                    {item.label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </nav>
         </header >
     );
 }
